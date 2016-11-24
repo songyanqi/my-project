9ffcd91ad9415e2a23a23982cf72db0a45422e7c
songyanqi <893210075@qq.com>
2016-11-24 15:42:47 +0800

    Add files via upload

---
+++ Tao-heaven/fis-conf.js
@@ -0,0 +1,20 @@
+
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+
+fis.match('*.html', {  
+  //invoke fis-optimizer-html-minifier  
+  optimizer: fis.plugin('html-minifier')  
+}); 
